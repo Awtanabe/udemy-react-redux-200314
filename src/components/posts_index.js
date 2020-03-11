3213cfb127845ee1d7e63375ab04cced3fa71b1a
Awtanabe <panser098@gmail.com>
2020-03-11 22:48:41 +0900

    fetchpostsさkすえい

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchPosts} from '../actions/index'
 
 class PostsIndex extends Component {
+  
+  handleClick(){
+    debugger
+    this.props.fetchPosts()
+  }
+  
   render() {
     return (
       <div>
+        <button onClick={() => this.handleClick()}>クリック</button>
         Post Index
       </div>
     )
   }
 }
 
-export default PostsIndex
+export default connect(null, {fetchPosts: fetchPosts})(PostsIndex)
