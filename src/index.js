98d01c55cc45b0f12a4d11bccabc47ef1ba183ee
Awtanabe <panser098@gmail.com>
2020-03-11 22:33:03 +0900

    add promise

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise'
 import {BrowserRouter, Route} from 'react-router-dom'
 
 // BrowserRouterはhistoryの役割
@@ -9,7 +10,7 @@ import {BrowserRouter, Route} from 'react-router-dom'
 import reducers from './reducers';
 import PostsIndex from './components/posts_index'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
