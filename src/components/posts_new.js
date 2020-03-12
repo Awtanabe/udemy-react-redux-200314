966aa6a9af0798d2e4cb531ff3cc8f017c51b189
Awtanabe <panser098@gmail.com>
2020-03-12 20:04:07 +0900

    redux form 初期設定

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
 
@@ -11,4 +12,13 @@ class PostsNew extends Component {
   }
 }
 
-export default PostsNew;
+export default reudxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
+
+
+// PostsEdit.js 編集でもこれが使えるみたい
+
+// export default reudxForm({
+//   form: 'PostsNewForm'
+// })(PostsNew);
