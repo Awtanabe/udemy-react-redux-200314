c2f38a2af3714bc79f29692a6e23694914c7e4c7
Awtanabe <panser098@gmail.com>
2020-03-12 20:15:10 +0900

    redux form 誤字 && Field作成

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,22 +3,40 @@ import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
 
+  renderTitleField(field){
+    return (
+      <div>
+        <input 
+          // onChange={field.input.onCHange}これらが..で含まれている
+　         type="text"
+          {...field.input}
+        />
+      </div>
+    )
+
+  }
   render() {
     return (
       <div>
         Posts new
+        <form>
+          <Field
+            name="title"
+            component={this.renderTitleField}
+          />
+        </form>
       </div>
     )
   }
 }
 
-export default reudxForm({
+export default reduxForm({
   form: 'PostsNewForm'
 })(PostsNew);
 
 
 // PostsEdit.js 編集でもこれが使えるみたい
 
-// export default reudxForm({
+// export default reduxForm({
 //   form: 'PostsNewForm'
 // })(PostsNew);
