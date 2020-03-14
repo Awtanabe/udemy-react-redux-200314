0c2efa48b9dcef2852a2ea309224b4d166d9bbe8
Awtanabe <panser098@gmail.com>
2020-03-14 17:45:57 +0900

    api のcakkback

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -54,9 +54,10 @@ class PostsNew extends Component {
 
   onSubmit(values){
     console.log(values)
-    
-    this.props.createPost(values)
-    this.props.history.push("/")
+
+    this.props.createPost(values, () =>{
+      this.props.history.push("/")
+    })
   }
 
   
